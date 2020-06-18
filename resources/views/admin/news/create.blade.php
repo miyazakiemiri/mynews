360edbb4f9f48bd1b63d67185a46253a7279dbed
miyazaki <toyodakiyosi20@gmail.com>
2020-06-18 03:22:32 +0000

    “PHP/Laravel_11”

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -7,14 +7,15 @@
         
         <title>MyNews</title>
     </head>
-{{-- layouts/admin.blade.phpを読み込む --}}
-@extends('layouts.admin')
+    <body>
+    {{-- layouts/admin.blade.phpを読み込む --}}
+    @extends('layouts.admin')
 
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成')
+    {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+    @section('title', 'ニュースの新規作成')
 
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
-@section('content')
+    {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+    @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
@@ -22,5 +23,6 @@
             </div>
         </div>
     </div>
-@endsection
+    @endsection
+</body>
 </html>
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,6 +8,21 @@
         <title>MyProfile</title>
     </head>
     <body>
-        <h1>Myプロフィール作成画面</h1>
+    {{-- layouts/admin.blade.phpを読み込む --}}
+    @extends('layouts.admin')
+
+    {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+    @section('title', 'プロフィールの新規画面')
+
+    {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+    @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+    @endsection
     </body>
 </html>
